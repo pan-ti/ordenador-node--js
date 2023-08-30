285d75982ffa31fdc75029b7aaa5c273faa26dfd
pan-ti <pandeandromeda@gmail.com>
2023-08-30 14:15:58 -0300

    função assíncrona chamada ordenarPropriedades.Cria uma cópia do array propriedades usando o operador spread [...propriedades].Utiliza o método .sort() para ordenar a cópia alfabeticamente, considerando caracteres especiais e acentos.Limpa o terminal utilizando a biblioteca clear-terminal.

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ import clear from 'clear-terminal';
 
 const propriedades = [];
 
+async function ordenarPropriedades() {
+    const propriedadesOrdenadas = [...propriedades].sort((a, b) => a.localeCompare(b, 'pt', { sensitivity: 'base' }));
+    
+    clear();
+    
+    console.log('Lista de propriedades ordenadas:');
+    propriedadesOrdenadas.forEach(propriedade => console.log(propriedade));
+}
