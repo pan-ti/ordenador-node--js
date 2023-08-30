671351ec6f357b85169bbd7a1b92c0b9211bab61
pan-ti <pandeandromeda@gmail.com>
2023-08-30 14:21:50 -0300

    criação de um loop que solicita propriedades ao usuário, armazena-as no array propriedades e continua solicitando até que o usuário insira sair. Quando isso acontece, as propriedades são ordenadas e impressas na tela.

--- app.js
+++ app.js
@@ -5,9 +5,21 @@ const propriedades = [];
 
 async function ordenarPropriedades() {
     const propriedadesOrdenadas = [...propriedades].sort((a, b) => a.localeCompare(b, 'pt', { sensitivity: 'base' }));
-    
+
     clear();
-    
+
     console.log('Lista de propriedades ordenadas:');
     propriedadesOrdenadas.forEach(propriedade => console.log(propriedade));
 }
+async function capturarPropriedades() {
+    while (true) {
+        const { propriedade } = await prompt.get(['propriedade']);
+        if (propriedade.toLowerCase() === 'sair') {
+            ordenarPropriedades();
+            break;
+        }
+        propriedades.push(propriedade);
+    }
+}
+
+capturarPropriedades();
