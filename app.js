a61fb1997985600cbc23bd0c89ffca64a2df54f7
pan-ti <pandeandromeda@gmail.com>
2023-08-30 14:31:51 -0300

    adição de cores ao terminal para deixar mais intuitivo ao usuário

--- app.js
+++ app.js
@@ -1,26 +1,41 @@
 import prompt from 'prompt-async';
 import clear from 'clear-terminal';
+import chalk from 'chalk';
 
 const propriedades = [];
 
 async function ordenarPropriedades() {
-    const propriedadesOrdenadas = [...propriedades].sort((a, b) => a.localeCompare(b, 'pt', { sensitivity: 'base' }));
+    const propriedadesOrdenadas = [...propriedades].sort((a, b) =>
+        a.localeCompare(b, 'pt', { sensitivity: 'base' })
+    );
 
     clear();
+    console.log(chalk.yellow('Lista de propriedades ordenadas:'));
 
-    console.log('Lista de propriedades ordenadas:');
-    propriedadesOrdenadas.forEach(propriedade => console.log(propriedade));
+    // Imprime a lista ordenada em verde
+    propriedadesOrdenadas.forEach((propriedade) =>
+        console.log(chalk.green(propriedade))
+    );
 }
+
 async function capturarPropriedades() {
     while (true) {
-        const { propriedade } = await prompt.get(['propriedade']);
+        const { propriedade } = await prompt.get([
+            {
+                name: 'propriedade',
+                description: chalk.cyan('Digite sua propriedade CSS (ou "SAIR" para finalizar)'),
+            },
+        ]);
+
         if (propriedade.toLowerCase() === 'sair') {
             ordenarPropriedades();
             break;
+        } else {
+            propriedades.push(propriedade);
         }
-        propriedades.push(propriedade);
     }
 }
-clear(); // Limpar o terminal
+
+clear();
 console.log(chalk.yellow('Bem-vindo ao Ordenador de Propriedades de CSS!'));
 capturarPropriedades();
